f0166c94e2252eaef3a92917fdfd2fb276cd1a6b
CynaraCosta <cvocac@cin.ufpe.br>
2022-08-17 20:52:33 -0300

    added the 6qst

--- 6qst/public/sketch.js
+++ 6qst/public/sketch.js
@@ -3,11 +3,9 @@ const green = [0, 255, 0]
 const blue = [0, 0, 255]
 const big_circle_d = 200; 
 const little_circle_d = 50;
-const angle_x_base = -Math.PI*2/3;
+const angle_x_base = -Math.PI/3;
 var rot_big_circle = 0;
 var rot_little_circle = 0;
-
-//const RAD = 6.28319
 
 function setup() {
     createCanvas(windowWidth - 15, windowHeight - 20, WEBGL);
@@ -18,46 +16,61 @@ function draw () {
     background('grey');
     orbitControl();
     
-    rotateX(Math.PI/3);
-    rotateY(-Math.PI/3);
-    rotateZ(Math.PI/3);
+    rotateX(Math.PI/3)
+    axis();
+    rotateX(-Math.PI/3)
 
     stroke("white");
     noFill();
     rect(0, 0, big_circle_d, -big_circle_d);
-
+    
     circle(big_circle_d/2, -big_circle_d/2, big_circle_d);
-   
-    push();
-    translate(big_circle_d/2, -big_circle_d);
-    rotateX(-Math.PI/2);
-    circle(0, - little_circle_d/2, little_circle_d);
-
+  
+    // transladar para o meio do quadrado
+    translate(big_circle_d/2, -big_circle_d/2, little_circle_d/2)
+  
+    // rotacionando o circulo pequeno no grande
+    rotateZ(rot_big_circle)
+    
+    // transladando para o centro do circulo grande
+    translate(0, -big_circle_d/2, 0)
+    // rotacionando o circulo pequeno perpendicular
+    rotateX(-Math.PI/2)
+  
+    circle(0, 0, little_circle_d)
+    
+    // desfazendo rotação do circulo pequeno perpendicular
+    rotateX(Math.PI/2)
+    
+    // rotação circulo pequeno bolinha
+    rotateY(rot_little_circle)
+    
     stroke(red);
     strokeWeight(8)
-    point(0, -little_circle_d);
+    point(0,0, little_circle_d/2)
     
-    rotate(rot_little_circle);
-    rot_little_circle += 1
+    // desfazendo rotação circulo pequeno bolinha
+    rotateY(-rot_little_circle)
+    rot_little_circle += (-Math.PI/60)
     
-    pop();
+    // desfazendo translação para o centro do circulo grande
+    translate(0, big_circle_d/2, 0)
     
-    rotateX(angle_x_base);
-
-    //rotate(rot_big_circle);
-
-    //rotate(rot_little_circle);
+    // desfazendo rotação o circulo pequeno no grande
+    rotateZ(-rot_big_circle)
+    rot_big_circle -= (Math.PI/120)
     
-    // considerando o movimento circular da bolinha na bola grande
-    //rot_big_circle -= (Math.PI/2*240);
-    // considerando o movimento circular da bolinha nela mesma
-    //rot_little_circle += (Math.PI/2*60);
-
-
-    axis();
+    // voltando eixo para o 0 0 0 
+    translate(-big_circle_d/2, big_circle_d/2, -little_circle_d/2)
+  
 }
 
+
 function axis(){
+    
+    // rotacionando para 60 o eixo Z 
+    rotateZ(Math.PI/3)
+  
     strokeWeight(3)
     // eixo x - vermelho
     stroke('rgba(255, 0, 0, 0.5)');
